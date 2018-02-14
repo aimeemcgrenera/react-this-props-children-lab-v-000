f747bf1a30bbb4617854fe60a7cdf854fc6e6edd
Aimee McGrenera <aimee.mcgrenera@gmail.com>
2018-02-14 22:10:16 +0000

    automatically backed up by learn

--- src/components/Invitation.js
+++ src/components/Invitation.js
@@ -3,8 +3,13 @@ import React from 'react';
 export default class Invitation extends React.Component {
   render() {
     return (
+<<<<<<< HEAD
       <div className="invitation">
         <h1>You've been invited!</h1>
+=======
+      <div className="Invitation">
+        <h1>"You've been invited!"</h1>
+>>>>>>> 86d712786749188a8a8cfda9f075e1bc676c1901
         <p>{this.props.children}</p>
       </div>
     );
--- src/components/ThemedDecorations.js
+++ src/components/ThemedDecorations.js
@@ -4,12 +4,21 @@ export default class ThemedDecorations extends React.Component {
   render() {
     const childrenWithWrapperDiv = React.Children.map(this.props.children, child => {
       return (
+<<<<<<< HEAD
         <div className={this.props.theme}>{child}</div> 
+=======
+        <div className="some-component-special-class">{child}</div> 
+>>>>>>> 86d712786749188a8a8cfda9f075e1bc676c1901
       );
     });
  
     return (
+<<<<<<< HEAD
       <div>
+=======
+      <div className="some-component">
+        <p>This component has {React.Children.count(this.props.children)} children.</p>
+>>>>>>> 86d712786749188a8a8cfda9f075e1bc676c1901
         {childrenWithWrapperDiv}        
       </div>      
     );
